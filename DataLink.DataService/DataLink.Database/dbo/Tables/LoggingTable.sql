ff5c84e4a8af9308bbb6ef7e6ff8e3df63671de8
robinson <letrunghieu2710@gmail.com>
2016-04-21 14:57:18 +0200

    logging database project

---
+++ DataLink.DataService/DataLink.Database/dbo/Tables/LoggingTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LoggingTable] (
+    [Id]        UNIQUEIDENTIFIER NOT NULL,
+    [Data]      NVARCHAR (500)   NOT NULL,
+    [TimeStamp] DATE             NOT NULL,
+    CONSTRAINT [PK_LoggingTable] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
